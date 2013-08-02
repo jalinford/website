860d9102286a2c62781c571526afbbe4aadb30b1
oleger814 <jandrewl814@yahoo.com>
2013-08-01 21:53:39 -0700

    blog and footer fix

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -10,7 +10,12 @@ class ResumeController < ApplicationController
   	@tags = Tag.where(featured: true).order("title ASC")
   	@all_tags = Tag.order("title ASC")
   	@tags_displayed = Tag.find_all_by_title(params[:title].split('/'))
-    @title = @tags_displayed.join(', ')
   	@featured_slideshows = Slideshow.where(featured: true).order("title ASC")
+
+    all_titles_array = []
+    @all_tags.each do |tag|
+      all_titles_array.push(tag.title)
+    end
+    @title = all_titles_array.sort.join('/')
   end
 end
