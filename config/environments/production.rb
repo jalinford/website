ed45e7091dccd7ea3aa6bb939cb2fa42c2b42ad1
oleger814 <jandrewl814@yahoo.com>
2013-08-22 00:58:03 -0700

    change production config for heroku 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,7 @@ Website::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  # CHANGED FOR HEROKU
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
