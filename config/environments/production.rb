bee1f06f805a6739bc67493a9501fc2e4ddfbc44
oleger814 <jandrewl814@yahoo.com>
2013-10-12 18:23:49 -0700

    compile fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Website::Application.configure do
-  config.initialize_on_precompile = false
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -10,14 +9,14 @@ Website::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # CHANGED FOR HEROKU
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -28,7 +27,7 @@ Website::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.action_dispatch.x_sendfile_header = nil # For Heroku
+  #config.action_dispatch.x_sendfile_header = nil # For Heroku
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -49,7 +48,7 @@ Website::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
